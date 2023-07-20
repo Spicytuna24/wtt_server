6127a15c15c8df075ea3e1455abd6147cd9bf1d7
Spicytuna24 <you@example.com>
2023-07-19 19:34:53 -0700

    finished image upload route

---
+++ server/routes/treeimages/treeimages-validations.js
@@ -0,0 +1,10 @@
+export default function validateImage(req) {
+    if (!req?.body) return false;
+    const {  treeimages = null } = req.body;
+    if (treeimages) {
+      if (treeimages?.idImage === undefined) return false;
+    }
+  
+    return true;
+  }
+  
